9c41f4628bb6c7e9a404547708644f3534935bfa
vkings1 <elmer.alluad@gmail.com>
2019-04-17 23:54:22 +0800

    Badge for linkedin

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 72)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  }); //end of scroll down
+
  //start of menu toggle
   $('.menu-toggle').click(function(){
     $('nav').toggleClass('active')
@@ -16,7 +31,7 @@ $(document).ready(function(){
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
     }, 500);
-  });
+  }); //end of scroll to top
 
 });//end of jquery
 
